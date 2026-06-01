d8c88e8691ebfb10c49a7afe87779d73f0a885af
adelavaidaa <adelavaida7@yahoo.com>
2026-06-02 00:17:11 +0300

    Update Sertar.java

---
+++ Sertar.java
@@ -0,0 +1,15 @@
+public class Sertar {
+    private int latime;
+    private int lungime;
+    private int inaltime;
+
+public Sertar(int latime, int lungime, int inaltime) {
+    this.latime = latime;
+    this.lungime = lungime;
+    this.inaltime = inaltime;
+    }
+
+public void tipareste() {
+    System.out.println("Sertar " + latime + " " + lungime + " " + inaltime);
+    }
+}
